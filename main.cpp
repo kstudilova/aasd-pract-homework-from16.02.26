3f3d485f52db084148a5eb0934c38ddfb595150e
kstudilova <kstudilova@gmail.com>
2026-03-06 00:08:29 +0300

    implicit interface

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+/*Недостатки явного интерфейса:
+1) Может быть выход за границы выходного массива,
+2) Нет гарантии, что входные массивы отсортированы
+*/
+
 template< class T >
 T* mergeN(const T* const* a, size_t sa, const size_t* sai, T* c)
 {
@@ -11,36 +16,36 @@ T* mergeN(const T* const* a, size_t sa, const size_t* sai, T* c)
   size_t totalSize = sai[0];
   for (size_t i = 0; i < totalSize; ++i)
   {
-    c[i] = a[0][i];
+    c[i] = a[0][i]; //operator=
   }
 
   for (size_t i = 1; i < sa; ++i)
   {
     T* tmp = nullptr;
     try {
-      tmp = new T[totalSize + sai[i]];
+      tmp = new T[totalSize + sai[i]]; //T::T()
       size_t k = 0;
       size_t indexC = 0;
       size_t indexA = 0;
       while (indexC < totalSize && indexA < sai[i])
       {
-        if (c[indexC] < a[i][indexA])
+        if (c[indexC] < a[i][indexA]) //operator<
         {
-          tmp[k++] = c[indexC++];
+          tmp[k++] = c[indexC++]; //operator=
         } else {
-          tmp[k++] = a[i][indexA++];
+          tmp[k++] = a[i][indexA++]; //operator=
         }
       }
       while (indexC < totalSize)
       {
-        tmp[k++] = c [indexC++];
+        tmp[k++] = c [indexC++]; //operator=
       }
       while (indexA < sai[i])
       {
-        tmp[k++] = a[i][indexA++];
+        tmp[k++] = a[i][indexA++]; //operator=
       }
       for (size_t j = 0; j < totalSize + sai[i]; ++j) {
-        c[j] = tmp[j];
+        c[j] = tmp[j]; //operator=
       }
       totalSize += sai[i];
       delete[] tmp;
